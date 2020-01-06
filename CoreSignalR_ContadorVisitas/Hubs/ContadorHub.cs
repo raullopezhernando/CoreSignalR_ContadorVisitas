b461220391d7b0669bdbbe3d45c6c495a3d05ed9
RaulLH <raullopezhernando@gmail.com>
2020-01-06 09:15:10 +0100

    Agregar archivos de proyecto.

---
+++ CoreSignalR_ContadorVisitas/Hubs/ContadorHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreSignalR_ContadorVisitas.Hubs
+{
+    public class ContadorHub : Hub
+    {
+        private static int _visitantes = 0;
+
+        // Los metodos ahora son asyncronos con lo cual tienen que tener async y await
+        //Notificamos a todos los clientes registrados
+        public override async Task OnConnectedAsync()
+        {
+            Interlocked.Increment(ref _visitantes);
+            await Clients.Others.SendAsync("displayMessage", "Nuevo conexión " + Context.ConnectionId + " Total Visistantes: " + _visitantes);
+            await Clients.Caller.SendAsync("displayMessage", "Hola, Bienvenido...!!!");
+        }
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            Interlocked.Decrement(ref _visitantes);
+
+            // Mandamos al cliente lo que recibe el servidor. Notificamos a todos los clientes registrados
+            await Clients.All.SendAsync("displayMessage", Context.ConnectionId + " cerro la conexión." + " Total Visitantes: " + _visitantes);
+        }
+
+        // Metodo que los clientes pueden invocar para enviar mensajes
+        public async Task BroadcastMessage(string message)
+        {
+            await Clients.All.SendAsync("displayMessage", Context.ConnectionId + ">>" + message);
+        }
+    }
+}
+
---
+++ CoreSignalR_ContadorVisitas/wwwroot/js/contador.js
@@ -0,0 +1,35 @@
+﻿
+// Establecemos conexion con el end point (/contadorHub). Para el login utilizamos el level information y 
+// con build construimos la conexion
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/contadorHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+// Iniciamos la conexion y metemos el catch para obtener los errores
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+// Connection.on le permite a hub invocar un metodo en el cliente. Nos viene solo el parametro message desde el hub
+
+connection.on("displayMessage", function (message) {
+    var li = document.createElement("li");
+    li.textContent = message;
+    li.className = "list-group-item";
+    document.getElementById("messages").appendChild(li);
+});
+
+// Codigo para interactuar con el metodo del hub "BroadcastMessage"
+document.getElementById("send").addEventListener("click", function (event) {
+    var message = document.getElementById("text").value;
+    //Para invokar al Hub
+    connection.invoke("BroadcastMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    // Una vez que se envia el mensaje borramos la caja de texto
+    document.getElementById("text").value = "";
+    //Habilitamos el evento preventDefault()
+    event.preventDefault();
+});
